e1c3368a579a0946994368c793403fe59de61f8e
Dave Harms <daveharmswebdev@gmail.com>
2016-02-28 22:56:28 -0600

    beautify and added font

--- js/app.js
+++ js/app.js
@@ -5,29 +5,31 @@ $(function() {
   $('li').on('click', function() {
     $('section').removeClass('showMainContent');
     var sectionToShow = $(this).attr('class');
-    var sectionToShow = sectionToShow.replace('--call','');
-    $('.'+sectionToShow).addClass('showMainContent');
+    var sectionToShow = sectionToShow.replace('--call', '');
+    $('.' + sectionToShow).addClass('showMainContent');
     $('nav ul').toggleClass('showing');
   })
   $('.displayContent').click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
     var video = $(this).attr('data-url');
-    if (video.substring(0,4)==='http') {
+    if (video.substring(0, 4) === 'http') {
       $(".videowrap").fadeIn(400);
-      $("#video").attr("src",video);
+      $("#video").attr("src", video);
     } else {
       $(".imagewrap").fadeIn(400);
-      $('.lowRentBox').attr('src',video).fadeIn(400);
+      $('.lowRentBox').attr('src', video).fadeIn(400);
     }
   })
-  $(".closebox, .videowrap, .imagewrap").click(function(){
+  $(".closebox, .videowrap, .imagewrap").click(function() {
     $(".videowrap, .imagewrap").fadeOut(400);
-    $("#video").attr("src","");
+    $("#video").attr("src", "");
   });
-  $(document).keydown(function(event){
-    if(event.which == 27){
+  $(document).keydown(function(event) {
+    if (event.which == 27) {
       $(".videowrap, .imagewrap").fadeOut(400);
-      $("#video").attr("src","");
+      $("#video").attr("src", "");
     }
   });
 })
